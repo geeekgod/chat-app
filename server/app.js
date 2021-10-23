f2ffc45c8a81c9fdb520083b6c03f89be2a2e600
Rishabh Singh <ris8645208@gmail.com>
2021-10-23 12:51:33 +0530

    chore: add server scripts and socket.io initialized

--- server/app.js
+++ server/app.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 app.use(cors());
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "POST"],
+  },
+});
+
 
 const port = process.env.PORT || 5000;
 
