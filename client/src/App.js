88451f0bc3b42ace621f38f1d2b7620cbe768190
Rishabh Singh <ris8645208@gmail.com>
2021-10-23 13:43:39 +0530

    style: add styles to join room

--- client/src/App.js
+++ client/src/App.js
@@ -19,23 +19,25 @@ const App = () => {
 
   return (
     <div className="App">
-      <h3>Join Chat</h3>
-      <input
-        type="text"
-        placeholder="John..."
-        onChange={(event) => {
-          setUsername(event.target.value);
-        }}
-      />
-      <input
-        type="text"
-        placeholder="Room ID..."
-        onChange={(event) => {
-          setRoom(event.target.value);
-        }}
-      />
-      <button onClick={joinRoom}>Join A Room</button>
-
+      <div className="joinChatContainer">
+        {" "}
+        <h3>Join Chat</h3>
+        <input
+          type="text"
+          placeholder="John..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Room ID..."
+          onChange={(event) => {
+            setRoom(event.target.value);
+          }}
+        />
+        <button onClick={joinRoom}>Join A Room</button>{" "}
+      </div>
       <Chat socket={socket} username={username} room={room} />
     </div>
   );
